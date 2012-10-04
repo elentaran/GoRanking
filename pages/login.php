fbdf8f194758f2703125339e4e4d35f02616397c
Arpad Rimmel <arpad.rimmel@free.fr>
2012-10-04 14:29:15 +0200

    add test javascript in login

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,16 @@
 <div id="right_widget">
+<script>
+function displayDate()
+{
+    document.getElementById("demo").innerHTML=Date();
+}
+</script>
+
     <p>login</p>
+
+    <p id="demo">This is a paragraph.</p>
+
+    <button type="button" onclick="displayDate()">Display Date</button>
     <p>test</p>
     <p>test</p>
     <p>test</p>
