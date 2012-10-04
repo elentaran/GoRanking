d969e57679151dcc7f3c274d20dad4807df587e4
Arpad Rimmel <arpad.rimmel@free.fr>
2012-10-04 14:05:31 +0200

    show the news from the database

--- pages/news.php
+++ pages/news.php
@@ -1,14 +1,22 @@
 
 <article>                
     <h1>NEWS</h1>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
-    <p>Bla bla bla bla (texte de l'article)</p>
+
+<?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=goRanking', 'root', '313ntaran');
+} catch(Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+    $reponse = $bdd->query('SELECT * FROM news ORDER BY date DESC LIMIT 0,5');
+    while ($donnees = $reponse->fetch()) {
+        echo "<div id=\"news\">\n";
+        echo "<h3>". $donnees['title']."</h3>\n";
+        echo "<p>". $donnees['news']."</p>\n";
+        echo "<p align=\"right\">". $donnees['author']." - ".$donnees['date']."</p>\n";
+        echo "</div>\n";
+    }
+    $reponse->closeCursor();
+?>
 </article>
