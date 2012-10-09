0fbffa12f9c129e8dca0796e7aaf953b58798fb3
Arpad Rimmel <arpad.rimmel@free.fr>
2012-10-09 11:32:01 +0200

    ajout de quelques liens

--- pages/links.php
+++ pages/links.php
@@ -1,5 +1,31 @@
 
 <article>                
     <h1>LINKS</h1>
-    <p> Under Construction </p>
+    <h2>Go Information</h2>
+    <ul>
+        <li><a href="http://senseis.xmp.net/">Go Sensei</a></li>
+        <p>A site with a lot of information about the game of Go.</p>
+        <li><a href="">Go News</a></li>
+    </ul>
+    
+    <h2>Go Servers</h2>
+    <ul>
+        <li><a href="www.gokgs.com/">KGS</a></li>
+        <p>A server with mainly non-asian players. Very user-friendly. Accessible on-line (with java). Allows bots to connect.</p>
+        <li><a href="www.pandanet-igs.com">IGS</a></li>
+        <p>One of the first go server. Broadcast some Pro Tournaments</p>
+        <li><a href="">Tygem</a></li>
+        <p> </p>
+        <li><a href="http://www.wbaduk.com/">WBaduk</a></li>
+        <p>A server with mainly asian players but it has an english client available. A lot of very hich level games.</p>
+    </ul>
+    
+    <h2>Other Links</h2>
+    <ul>
+        <li><a href="http://www.teamliquid.net/">TeamLiquid</a></li>
+        <p>A site with all the information about the competitive Starcraft 2 and Dota 2 scene. It was the inspiration for this website.</p>
+        <li><a href="http://jinix.sourceforge.net/go/english.htm">SwiftGo</a></li>
+        <p>An android application that allows to connect to the IGS and Tygem servers and watch go games on your phone (you need to have an account to connect to Tygem).</p>
+    </ul>
+    
 </article>
